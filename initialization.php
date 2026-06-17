e0bdb976243e714763230a521df16a13de768bf8
mikeyperes <mikeyperes@users.noreply.github.com>
2026-06-17 00:14:12 +0000

    Register HWS user social ACF fields

--- initialization.php
+++ initialization.php
@@ -261,7 +261,7 @@ $plugin_name = "Hexa Web Systems - Website Base Tool";
 $plugin_description = "Basic tools for optimization, performance, and debugging on Hexa based web systems.";
 $author_name = "Michael Peres";
 $plugin_uri = "https://github.com/mikeyperes/hws-base-tools";
-$plugin_version = "10.18.22";
+$plugin_version = "10.18.23";
 $author_uri = "https://michaelperes.com";
 $api_url = "https://api.github.com/repos/mikeyperes/hws-base-tools";
 $plugin_github_url = "https://github.com/mikeyperes/hws-base-tools";
@@ -346,6 +346,9 @@ include_once("smp-core/register-post-type-team-member.php");
 include_once("smp-core/register-post-type-testimonial.php");
 
 include_once("smp-core/register-acf-user.php");
+if ( function_exists( '\\enable_smp_acf_user' ) ) {
+    \enable_smp_acf_user();
+}
 include_once("smp-core/register-acf-organization.php");
 include_once("smp-core/register-acf-team-member.php");
 include_once("smp-core/register-acf-testimonial.php");
